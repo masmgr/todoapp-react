fea96b52b8c8f9ac45f53fe92bcc00f61512090d
Masaki Ikeda <imasaki20@gmail.com>
2022-07-13 02:10:36 +0900

    Use mui.

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,44 +1,26 @@
 import React from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-
-import { Button } from "./Button";
+import { MyButton } from "../App";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Example/Button",
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  component: Button,
+  component: MyButton,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: "color" },
   },
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof MyButton>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 // eslint-disable-next-line react/function-component-definition, react/jsx-props-no-spreading
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof MyButton> = () => <MyButton />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: "Button",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
   label: "Button",
 };
